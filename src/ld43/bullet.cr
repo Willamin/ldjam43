cb6fdac2bf535a13d8c050545b8833046e14a301
Will Lewis <will@wflewis.com>
2018-12-02 13:45:09 -0500

    Destroy bullets outside the window

--- src/ld43/bullet.cr
+++ src/ld43/bullet.cr
@@ -10,17 +10,20 @@ class Bullet < Entity
     when Player::LEFT  then @x -= (speed * dt).to_i
     when Player::RIGHT then @x += (speed * dt).to_i
     end
+
+    if @x < -2.tiles || @y < -2.tiles || @x > 17.tiles || @y > 17.tiles
+      @delete_me = true
+    end
   end
 
   def collision(m : Monster)
     m.delete_me = true
-    self.delete_me = true
+    @delete_me = true
     Molly.play_sound(Molly.load_sound("res/kill.wav"))
   end
 
   def draw
     Molly.set_color(Color.new(240, 240, 240))
     Molly.draw_rect(@x, @y, 1.tiles / 4, 1.tiles / 4)
-    # Molly.draw_sprite(@x, @y, Molly.load_sprite(@direction))
   end
 end
